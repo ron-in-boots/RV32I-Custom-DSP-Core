cf1a1a8ef5db8b3ac3833ba87c0a2d443ff3bfbb
ron-in-boots <gargaayan20@gmail.com>
2026-04-14 14:40:53 +0530

    Restore custom luftALU multiplier and RTL logic

--- Custom_DSP_Core/luftALU/rtl/alu.v
+++ Custom_DSP_Core/luftALU/rtl/alu.v
@@ -1,104 +1,70 @@
-// ALU of the CPU
+// ALU of the CPU - UPGRADED FOR SATURATING ARITHMETIC (Phase 1) -Upgraded for MAC (Phase 2)
 // Created:     2024-01-17
-// Modified:    2025-06-28
-// Author:      Kagan Dikmen
+// Modified:    2026-04-11
+// Author:      Kagan Dikmen / Aayan
 
 `include "./subunits/adder.v"
 `include "./subunits/comparison_unit.v"
 `include "./subunits/logic_unit.v"
 `include "./subunits/mux.v"
 `include "./subunits/shifter.v"
+`include "./subunits/multiplier.v" 
 
 module alu
     #(
     parameter OPERAND_LENGTH = 32
     )(
+    input clk,
+    input rst,
     input [OPERAND_LENGTH-1:0] opd1,
     input [OPERAND_LENGTH-1:0] opd2,
     input [OPERAND_LENGTH-1:0] opd3,
     input [OPERAND_LENGTH-1:0] opd4,
+    input [OPERAND_LENGTH-1:0] acc_in,  // NEW: 3rd port for MAC
     input cu_input_sel,
     input [1:0] subunit_res_sel,
     input [3:0] subunit_op_sel,
-    
+    input sat_en,                     
+    input mac_en,                       // NEW: Turn on multiplier
     output [OPERAND_LENGTH-1:0] alu_result,
-    output [OPERAND_LENGTH-1:0] comp_result   // zero-extended
+    output [OPERAND_LENGTH-1:0] comp_result 
     );
 
-    wire [OPERAND_LENGTH-1:0] adder_input1;
     wire [OPERAND_LENGTH-1:0] adder_result;
-    
-    wire [OPERAND_LENGTH-1:0] cu_input1;   // cu stands for "compare unit"
-    wire [OPERAND_LENGTH-1:0] cu_input2;
+    wire [OPERAND_LENGTH-1:0] mult_product;
+    wire [OPERAND_LENGTH-1:0] cu_input1, cu_input2, logic_result, shifter_result;
 
-    wire [OPERAND_LENGTH-1:0] logic_result;
-    wire [OPERAND_LENGTH-1:0] shifter_result;
+    // MUX Logic: If MAC, add (Product + Accumulator). Otherwise, (opd1 + opd2).
+    wire [OPERAND_LENGTH-1:0] adder_opd1 = (mac_en) ? mult_product : opd1;
+    wire [OPERAND_LENGTH-1:0] adder_opd2 = (mac_en) ? acc_in : opd2;
 
+    multiplier #(.OPERAND_LENGTH(OPERAND_LENGTH)) mult_in_alu (
+        .clk(clk), .rst(rst), .en(mac_en),
+        .a(opd1), .b(opd2), .product(mult_product), .done()
+    );
 
-    adder #(.OPERAND_LENGTH(OPERAND_LENGTH)) 
-                    adder_in_alu
-                    (
-                        .opd1(opd1),
-                        .opd2(opd2),
-                        .alu_op_select(subunit_op_sel),
-                        .adder_result(adder_result)
-                    );
+    adder #(.OPERAND_LENGTH(OPERAND_LENGTH)) adder_in_alu (
+        .opd1(adder_opd1), .opd2(adder_opd2), .alu_op_select(subunit_op_sel),
+        .sat_en(sat_en), .adder_result(adder_result)
+    );
 
-    comparison_unit #(.OPD_LENGTH(OPERAND_LENGTH)) 
-                    comparison_unit_in_alu 
-                    (
-                        .opd1(cu_input1),
-                        .opd2(cu_input2),
-                        .alu_op_select(subunit_op_sel),
-                        .comp_result(comp_result)
-                    );
+    comparison_unit #(.OPD_LENGTH(OPERAND_LENGTH)) comparison_unit_in_alu (
+        .opd1(cu_input1), .opd2(cu_input2), .alu_op_select(subunit_op_sel), .comp_result(comp_result)
+    );
 
-    logic_unit #(.OPD_LENGTH(OPERAND_LENGTH)) 
-                    logic_unit_in_alu 
-                    (
-                        .opd1(opd1),
-                        .opd2(opd2),
-                        .alu_op_select(subunit_op_sel),
-                        .logic_result(logic_result)
-                    );
+    logic_unit #(.OPD_LENGTH(OPERAND_LENGTH)) logic_unit_in_alu (
+        .opd1(opd1), .opd2(opd2), .alu_op_select(subunit_op_sel), .logic_result(logic_result)
+    );
 
-    shifter #(.OPD_LENGTH(OPERAND_LENGTH)) 
-                    shifter_in_alu 
-                    (
-                        .opd1(opd1),
-                        .opd2(opd2),
-                        .alu_op_select(subunit_op_sel),
-                        .shifter_result(shifter_result)
-                    );
+    shifter #(.OPD_LENGTH(OPERAND_LENGTH)) shifter_in_alu (
+        .opd1(opd1), .opd2(opd2), .alu_op_select(subunit_op_sel), .shifter_result(shifter_result)
+    );
 
+    two_input_mux #(.INPUT_LENGTH(OPERAND_LENGTH)) cu_mux1 (.a(opd1), .b(opd3), .sel(cu_input_sel), .z(cu_input1));
+    two_input_mux #(.INPUT_LENGTH(OPERAND_LENGTH)) cu_mux2 (.a(opd2), .b(opd4), .sel(cu_input_sel), .z(cu_input2));
 
-    two_input_mux #(.INPUT_LENGTH(OPERAND_LENGTH)) 
-                    cu_mux1
-                    (
-                        .a(opd1),
-                        .b(opd3),
-                        .sel(cu_input_sel),
-                        .z(cu_input1)
-                    );
-
-    two_input_mux #(.INPUT_LENGTH(OPERAND_LENGTH)) 
-                    cu_mux2
-                    (
-                        .a(opd2),
-                        .b(opd4),
-                        .sel(cu_input_sel),
-                        .z(cu_input2)
-                    );
-
-    four_input_mux #(.INPUT_LENGTH(OPERAND_LENGTH))
-                    result_mux
-                    (
-                        .a(adder_result),
-                        .b(logic_result),
-                        .c(shifter_result),
-                        .d(comp_result),
-                        .sel(subunit_res_sel),
-                        .z(alu_result)
-                    );
-    
+    four_input_mux #(.INPUT_LENGTH(OPERAND_LENGTH)) result_mux (
+        .a(adder_result), .b(logic_result), .c(shifter_result), .d(comp_result),
+        .sel(subunit_res_sel), .z(alu_result)
+    );
 endmodule
--- Custom_DSP_Core/luftALU/rtl/subunits/adder.v
+++ Custom_DSP_Core/luftALU/rtl/subunits/adder.v
@@ -1,7 +1,7 @@
-// Adder of the ALU
+// Adder of the ALU - UPGRADED FOR SATURATING ARITHMETIC (Phase 1)
 // Created:     2024-01-17
-// Modified:    2024-08-15 (last status: working fine)
-// Author:      Kagan Dikmen
+// Modified:    2026-04-11
+// Author:      Kagan Dikmen / Aayan
 
 `include "./adder_unit.v"
 
@@ -12,11 +12,15 @@ module adder
     input [OPERAND_LENGTH-1:0] opd1,
     input [OPERAND_LENGTH-1:0] opd2,
     input [3:0] alu_op_select,      // 0000 for addition, 1000 for subtraction
+    input sat_en,                   // NEW: Saturation enable flag
     
     output [OPERAND_LENGTH-1:0] adder_result
     );
 
     wire [OPERAND_LENGTH-1:0] c_out;
+    
+    // NEW: Intercept the raw calculation before sending it to the output
+    wire [OPERAND_LENGTH-1:0] raw_sum; 
 
     genvar i;
 
@@ -28,7 +32,7 @@ module adder
                         .a(opd1[i]), 
                         .b(alu_op_select[3] ^ opd2[i]), 
                         .c_in(alu_op_select[3]), 
-                        .out(adder_result[i]), 
+                        .out(raw_sum[i]),      // Changed from adder_result
                         .c_out(c_out[i])
                         );
         end
@@ -39,10 +43,35 @@ module adder
                         .a(opd1[i]), 
                         .b(alu_op_select[3] ^ opd2[i]), 
                         .c_in(c_out[i-1]), 
-                        .out(adder_result[i]), 
+                        .out(raw_sum[i]),      // Changed from adder_result
                         .c_out(c_out[i])
                         );
         end
     endgenerate
+
+    // =========================================================================
+    // SATURATION LOGIC
+    // =========================================================================
+    
+    // 1. Find the effective sign of Operand 2. (Inverted if subtracting)
+    wire opd2_eff_sign = opd2[OPERAND_LENGTH-1] ^ alu_op_select[3];
+    
+    // 2. Check for Overflow: Occurs if opd1 and effective opd2 have the SAME sign, 
+    // but the raw_sum has a DIFFERENT sign.
+    wire overflow = ~(opd1[OPERAND_LENGTH-1] ^ opd2_eff_sign) & (opd1[OPERAND_LENGTH-1] ^ raw_sum[OPERAND_LENGTH-1]);
+    
+    // 3. Determine if we exceeded the Max Positive or Min Negative bounds
+    wire is_pos_overflow = overflow & ~opd1[OPERAND_LENGTH-1];
+    wire is_neg_overflow = overflow & opd1[OPERAND_LENGTH-1];
+    
+    // 32-bit Boundary Constants
+    wire [OPERAND_LENGTH-1:0] MAX_POS = 32'h7FFFFFFF;
+    wire [OPERAND_LENGTH-1:0] MIN_NEG = 32'h80000000;
+    
+    // 4. Output Multiplexer
+    // If sat_en is high AND we overflowed, clamp it. Otherwise, output the normal math.
+    assign adder_result = (sat_en & is_pos_overflow) ? MAX_POS :
+                          (sat_en & is_neg_overflow) ? MIN_NEG :
+                          raw_sum;
 
 endmodule
